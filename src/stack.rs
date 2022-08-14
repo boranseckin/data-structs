0a937fa1eb12a7911c0f0d92b422015a97e506fc
Boran Seckin <boran@boranseckin.com>
2022-08-14 14:01:43 -0400

    Add generic type and tests for stack

--- src/stack.rs
+++ src/stack.rs
@@ -1,30 +1,30 @@
 #[derive(Default, Debug)]
-pub struct Stack {
-  list: Vec<i32>,
+pub struct Stack<T> {
+  list: Vec<T>,
   size: usize,
 }
 
-impl Stack {
-  pub fn new() -> Stack {
+impl<T: std::clone::Clone> Stack<T> {
+  pub fn new() -> Self {
     Stack {
-      list: vec![],
+      list: Vec::new(),
       size: 0,
     }
   }
 
-  pub fn from(values: &Vec<i32>) -> Stack {
+  pub fn from(values: &mut [T]) -> Stack<T> {
     Stack {
-      list: values.clone(),
+      list: values.to_vec(),
       size: values.len(),
     }
   }
 
-  pub fn push(&mut self, value: i32) {
+  pub fn push(&mut self, value: T) {
     self.list.push(value);
     self.size += 1;
   }
 
-  pub fn pop(&mut self) -> Option<i32> {
+  pub fn pop(&mut self) -> Option<T> {
     let result = self.list.pop();
 
     match result {
@@ -36,15 +36,68 @@ impl Stack {
     }
   }
 
-  pub fn size(&mut self) -> usize {
+  pub fn size(&self) -> usize {
     self.size
   }
 
-  pub fn peek(&mut self) -> Option<i32> {
+  pub fn peek(&self) -> Option<&T> {
     match self.size {
       0 => None,
-      _ => self.list.last().copied(),
+      _ => self.list.last(),
     }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::Stack;
+
+  #[test]
+  fn basics_new() {
+    let mut stack: Stack<i32> = Stack::new();
+
+    assert_eq!(stack.size(), 0);
+    assert_eq!(stack.peek(), None);
+    assert_eq!(stack.pop(), None);
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.peek(), Some(&3));
+    assert_eq!(stack.pop(), Some(3));
+
+    assert_eq!(stack.size(), 2);
+    assert_eq!(stack.peek(), Some(&2));
+    assert_eq!(stack.pop(), Some(2));
+
+    stack.push(4);
+    stack.push(5);
+
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.peek(), Some(&5));
+    assert_eq!(stack.pop(), Some(5));
+  }
+
+  #[test]
+  fn basics_from() {
+    let mut stack: Stack<i32> = Stack::from(&mut [1, 2, 3]);
+
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.peek(), Some(&3));
+    assert_eq!(stack.pop(), Some(3));
+
+    assert_eq!(stack.size(), 2);
+    assert_eq!(stack.peek(), Some(&2));
+    assert_eq!(stack.pop(), Some(2));
+
+    stack.push(4);
+    stack.push(5);
+
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.peek(), Some(&5));
+    assert_eq!(stack.pop(), Some(5));
   }
 }
 
